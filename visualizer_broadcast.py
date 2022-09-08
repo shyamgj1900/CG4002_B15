5de78c65e64e76824abb91c12526ac981b814864
Shyam <e0544484@u.nus.edu>
2022-09-08 23:27:20 +0800

    fix visualizer broadcast

--- visualizer_broadcast.py
+++ visualizer_broadcast.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 import paho.mqtt.client as mqtt
 import threading
 
@@ -9,7 +12,8 @@ class VisualizerBroadcast(threading.Thread):
         self.broker_address = "b1386744d1594b29a88d72d9bab70fbe.s1.eu.hivemq.cloud"
         self.username = "cg4002_b15"
         self.password = "CG4002_B15"
-        self.topic = ["Ultra96/visualizer, Ultra96/AI"]
+        self.topic = "Ultra96/visualizer"
+        self.init_message_queue_connection()
 
     def init_message_queue_connection(self):
         self.publisher.on_connect = self.on_connect
@@ -31,4 +35,12 @@ class VisualizerBroadcast(threading.Thread):
         print("Received Message: " + msg.topic + "->" + msg.payload.decode("utf-8"))
 
     def publish_message(self, message):
-        self.publisher.publish(self.topic[0], message)
+        self.publisher.publish(self.topic, message)
+
+
+# if __name__ == "__main__":
+#     vis_broad = VisualizerBroadcast()
+#     while True:
+#         data = input("type: ")
+#         print(data)
+#         vis_broad.publish_message(data)
