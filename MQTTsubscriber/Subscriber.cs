ade676e51469d147e1f0d4f30e94c74abc27b74b
Shyam <e0544484@u.nus.edu>
2022-09-15 18:48:53 +0800

    send random response

--- MQTTsubscriber/Subscriber.cs
+++ MQTTsubscriber/Subscriber.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Packets;
-
+using MQTTnet.Protocol;
 
 namespace MQTTsubscriber
 {
@@ -14,8 +14,9 @@ namespace MQTTsubscriber
         static async Task Main(string[] args)
         {
             var mqttFactory = new MqttFactory();
+            var message = "";
 
-            using (var mqttClient = mqttFactory.CreateMqttClient())
+            using (IMqttClient mqttClient = mqttFactory.CreateMqttClient())
             {
                 var mqttClientOptions = new MqttClientOptionsBuilder()
                     .WithCredentials("cg4002_b15", "CG4002_B15")
@@ -24,29 +25,50 @@ namespace MQTTsubscriber
                     .WithCleanSession()
                     .Build();
 
-                Console.Write("Finished auth");
-                
-                mqttClient.ApplicationMessageReceivedAsync += e =>
-                {
-                    Console.WriteLine($"Received application message. -> {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
-                    //e.DumpToConsole();
+                Console.WriteLine("Finished auth");
 
-                    return Task.CompletedTask;
+                mqttClient.ApplicationMessageReceivedAsync += async e =>
+                {
+                    message = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                    Console.WriteLine($"Received application message. -> {message}");
+
+                    if (message == "grenade")
+                    {
+                        await PublishMessageasync(mqttClient);
+                    }
                 };
 
                 var response = await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
                 Console.WriteLine(response);
 
                 var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
-                    .WithTopicFilter(f => { f.WithTopic("Ultra96/visualizer"); })
+                    .WithTopicFilter(f => { f.WithTopic("Ultra96/visualizer/receive"); })
                     .Build();
 
                 await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
 
                 Console.WriteLine("MQTT client subscribed to topic.");
 
+
                 Console.WriteLine("Press enter to exit.");
                 Console.ReadLine();
+            }
+        }
+
+        private static async Task PublishMessageasync(IMqttClient client)
+        {
+            Random rnd = new Random();
+            var idx = rnd.Next(0, 2);
+            string[] payload = new string[] {"yes", "no"};
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic("Ultra96/visualizer/send")
+                .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+                .WithPayload(payload[idx])
+                .Build();
+
+            if (client.IsConnected)
+            {
+                await client.PublishAsync(message);
             }
         }
     }
