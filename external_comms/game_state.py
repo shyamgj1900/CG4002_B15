8899a0fc319fc3d6fe3f97e3fa588c2464d52f26
Shyam <e0544484@u.nus.edu>
2022-10-23 14:48:41 +0800

    change game state to 1 player

--- external_comms/game_state.py
+++ external_comms/game_state.py
@@ -13,15 +13,15 @@ class GameState:
         data = {'p1': self.player1.get_dict(), 'p2': self.player2.get_dict()}
         return data
 
-    def detected_game_state(self, p1_action, p2_action):
+    def detected_game_state(self, new_action):
         """
         This function updates the players game state based on the action it has received from the client side. (For now
         this function only updates game state based on 1 player mode).
         """
-        player1_valid = self.player1.action_is_valid(p1_action)
-        player2_valid = self.player2.action_is_valid(p2_action)
-        self.player1.update(p1_action, p2_action, player1_valid, player2_valid)
-        self.player2.update(p2_action, p1_action, player2_valid, player1_valid)
+        player1_valid = self.player1.action_is_valid(new_action)
+        # player2_valid = self.player2.action_is_valid(new_action)
+        self.player1.update(new_action, 'none', player1_valid, False)
+        self.player2.update('none', new_action, False, player1_valid)
 
     def update_game_state(self, updated_state):
         player1_dict = updated_state['p1']
